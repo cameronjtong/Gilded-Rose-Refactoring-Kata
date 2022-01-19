a2e90ff0d237bb044220bbb12cc79b4ecc404f93
Cameron Tong <cameron.tong2b@gmail.com>
2022-01-19 12:42:58 -0500

    extract #sulfuras_tick

--- Ruby/lib/gilded_rose.rb
+++ Ruby/lib/gilded_rose.rb
@@ -13,6 +13,8 @@ class GildedRose
         return normal_tick
       when 'Aged Brie'
         return aged_tick
+      when "Sulfuras, Hand of Ragnaros"
+        return sulfuras_tick
     end
     if @name != "Aged Brie" and @name != "Backstage passes to a TAFKAL80ETC concert"
       if @quality > 0
@@ -72,5 +74,8 @@ class GildedRose
     @quality += 1
     return if quality == 50
     @quality += 1 if days_remaining < 0
+  end
+
+  def sulfuras_tick
   end
 end
