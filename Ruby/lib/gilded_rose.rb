271aa7eef7de207706f02c3496f37052d8d34e15
Cameron Tong <cameron.tong2b@gmail.com>
2022-01-19 13:05:09 -0500

    move class logic to initialize

--- Ruby/lib/gilded_rose.rb
+++ Ruby/lib/gilded_rose.rb
@@ -4,24 +4,17 @@ require_relative 'aged_brie'
 require_relative 'sulfuras'
 require_relative 'backstage'
 
+SPECIALIZED_ITEMS = {
+  'Aged Brie' => AgedBrie,
+  'Sulfuras, Hand of Ragnaros' => Sulfuras,
+  'Backstage passes to a TAFKAL80ETC concert' => Backstage
+}
+
 class GildedRose
   attr_reader :item
 
   def initialize(name:, days_remaining:, quality:)
-    @item = klass_for(name).new(quality, days_remaining)
-  end
-
-  def klass_for(name)
-    case name
-      when 'Normal Item'
-        NormalItem
-      when 'Aged Brie'
-        AgedBrie
-      when "Sulfuras, Hand of Ragnaros"
-        Sulfuras
-      when "Backstage passes to a TAFKAL80ETC concert"
-        Backstage
-    end
+    @item = (SPECIALIZED_ITEMS[name] || NormalItem).new(quality, days_remaining)
   end
 
   def tick
@@ -29,12 +22,10 @@ class GildedRose
   end
 
   def quality
-    return item.quality if item
-    @quality
+    item.quality
   end
 
   def days_remaining
-    return item.days_remaining if item
-    @days_remaining
+    item.days_remaining
   end
 end
