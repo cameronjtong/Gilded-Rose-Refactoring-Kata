7f01caf2cb1cbc7efdb055db63a8b857d6510fce
Cameron Tong <cameron.tong2b@gmail.com>
2022-01-19 13:09:01 -0500

    implement ConjuredMana

---
+++ Ruby/lib/conjured_mana.rb
@@ -0,0 +1,8 @@
+class ConjuredMana < Item
+  def tick
+    @days_remaining -= 1
+    return if quality == 0
+    @quality -= 2
+    @quality -= 2 if days_remaining < 0
+  end
+end
--- Ruby/lib/gilded_rose.rb
+++ Ruby/lib/gilded_rose.rb
@@ -3,11 +3,13 @@ require_relative 'normal_item'
 require_relative 'aged_brie'
 require_relative 'sulfuras'
 require_relative 'backstage'
+require_relative 'conjured_mana'
 
 SPECIALIZED_ITEMS = {
   'Aged Brie' => AgedBrie,
   'Sulfuras, Hand of Ragnaros' => Sulfuras,
-  'Backstage passes to a TAFKAL80ETC concert' => Backstage
+  'Backstage passes to a TAFKAL80ETC concert' => Backstage,
+  'Conjured Mana Cake' => ConjuredMana
 }
 
 class GildedRose
